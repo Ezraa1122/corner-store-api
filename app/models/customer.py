1cc830ef5d69c0f979cf2b84ea231160aa1c78bf
Ezra1122 <runnathezra11@gmail.com>
2026-07-29 18:00:58 +0200

    fixed typos in tables

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from app.database import Base
 
 class Customer(Base):
-    __tablename__ = "cutomers"
+    __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
